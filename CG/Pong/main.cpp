6c4d9417dde7f23aa8b5a36797859619c31c9bc8
mordekayo <karas.gangster@yandex.ru>
2023-03-05 13:53:37 +0300

    Removed addition of blank gameobject

--- CG/Pong/main.cpp
+++ CG/Pong/main.cpp
@@ -1,13 +1,9 @@
 #include "PongGame.h"
-#include "../GameFramework/Game.h"
-#include "../GameFramework/GameObjects/GameObject.h"
-#include "../GameFramework/Components/RenderComponent.h"
 
 int main()
 {
     PongGame* MyGame = PongGame::Instance();
-    FGameObject* GameObject = new FGameObject();
-    GameObject->AddComponent(new FRenderComponent());
-    MyGame->AddGameObject(GameObject);
     MyGame->Run();
+
+    return 0;
 }
