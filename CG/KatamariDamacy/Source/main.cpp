5b511243b55735e3fded8ec49491c5d893d2dd69
mordekayo <karas.gangster@yandex.ru>
2023-03-13 13:20:21 +0300

    Setted up Assimp

--- CG/KatamariDamacy/Source/main.cpp
+++ CG/KatamariDamacy/Source/main.cpp
@@ -2,20 +2,14 @@
 
 #include "KatamariGame.h"
 
-#include <assimp/Importer.hpp> 
-#include <assimp/scene.h> 
-#include <assimp/postprocess.h>
-
-
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "DirectXTK.lib")
+
 
 int main()
 {
     KatamariGame* MyGame = KatamariGame::Instance();
     MyGame->Run();
-
-    //Assimp::Importer importer;
 
     return 0;
 }
